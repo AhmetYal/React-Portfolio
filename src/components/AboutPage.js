e038b8e9e80d933abcfce858344d23a7a1f4bd02
ahmet yalçınkaya <a.yalcinkaya0@gmail.com>
2022-02-15 13:14:00 +0300

    s2

---
+++ src/components/AboutPage.js
@@ -0,0 +1,96 @@
+import React from "react";
+import styled, { keyframes, ThemeProvider } from "styled-components";
+import { DarkTheme } from "./Themes";
+
+import LogoComponent from "../subComponents/LogoComponent";
+import SocialIcons from "../subComponents/SocialIcons";
+import PowerButton from "../subComponents/PowerButton";
+import ParticleComponent from "../subComponents/ParticleComponent";
+import BigTitle from "../subComponents/BigTitlte";
+import astronaut from "../assets/Images/spaceman.png";
+
+const Box = styled.div`
+  background-color: ${(props) => props.theme.body};
+  width: 100vw;
+  height: 100vh;
+  position: relative;
+  overflow: hidden;
+`;
+const float = keyframes`
+0% { transform: translateY(-10px) }
+50% { transform: translateY(15px) translateX(15px) }
+100% { transform: translateY(-10px) }
+
+`;
+const Spaceman = styled.div`
+  position: absolute;
+  top: 10%;
+  right: 5%;
+  width: 20vw;
+  animation: ${float} 4s ease infinite;
+  img {
+    width: 100%;
+    height: auto;
+  }
+`;
+const Main = styled.div`
+  border: 2px solid ${(props) => props.theme.text};
+  color: ${(props) => props.theme.text};
+  padding: 2rem;
+  width: 50vw;
+  height: 60vh;
+  z-index: 3;
+  line-height: 1.5;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: calc(0.6rem + 0.5vw);
+  backdrop-filter: blur(4px);
+
+  position: absolute;
+  left: calc(5rem + 5vw);
+  top: 10rem;
+  font-family: "Ubuntu Mono", monospace;
+  font-style: italic;
+`;
+
+const AboutPage = () => {
+  return (
+    <ThemeProvider theme={DarkTheme}>
+      <Box>
+        <LogoComponent theme="dark" />
+        <SocialIcons theme="dark" />
+        <PowerButton />
+        <ParticleComponent theme="dark" />
+
+        <Spaceman>
+          <img src={astronaut} alt="spaceman" />
+        </Spaceman>
+        <Main>
+          Hi..My name is Ahmet.I'm a front-end developer located in Turkey. I
+          studied Management Information Systems.
+          <br /> <br />
+          I started the software with C#. I started developing Web Projects with
+          .NET Framework. Then I've been interested in React.js lately. I worked
+          very hard ı discovered new things, get some courses and certificates.
+          I have been in the IT industry for more than 2 years and I am
+          currently a Web Developer on my team.
+          <br /> <br />
+          I am good at Frontend Development especially with React, Js and
+          Bootstrap. I have experience with multi-layer software architectures
+          and Design Patterns. I have experience with database model binding
+          with Microsoft SQL Server,MongoDb and PostgreSql. I have knowledge of
+          HTML, CSS, JavaScript. I am familiar with Kanban and Scrum
+          environments.
+          <br /> <br />
+          I'm interested in the whole frontend stack Like trying new things and
+          building great projects. You can connect with me via social links.
+        </Main>
+
+        <BigTitle text="ABOUT" top="10%" left="5%" />
+      </Box>
+    </ThemeProvider>
+  );
+};
+
+export default AboutPage;
