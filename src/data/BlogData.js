6135f690339f128e2f683add9c279c0d95fa69de
ahmet yalçınkaya <a.yalcinkaya0@gmail.com>
2022-03-11 09:09:19 +0300

    se

--- src/data/BlogData.js
+++ src/data/BlogData.js
@@ -9,4 +9,12 @@ export const Blogs = [
     imgSrc: "https://miro.medium.com/max/875/1*S49BKR0Ylss9BmepNeomrg.png",
     link: "https://medium.com/@a.yalcinkaya0/front-end-developer-roadmap-react-3b06ed71bd8d",
   },
+  {
+    id: 2,
+    name: "10 Useful React NPM Packages For Your Next Project",
+    tags: ["react", "Front-End", "Development"],
+    date: "08 Feb, 2022",
+    imgSrc: "https://miro.medium.com/max/1400/1*snHX_luEHNqDGUQivb9P1A.jpeg",
+    link: "https://medium.com/@a.yalcinkaya0/10-useful-react-npm-packages-for-your-next-project-158325bc8a47",
+  },
 ];
